891b0cd90ed13e01f9a988072c532e307ca8d4fc
澤井聖也 <sawaiseiya@sawaikiyoshinari-no-MacBook-Pro.local>
2015-06-15 19:54:50 +0900

    first commit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var label:UILabel!
+    
+    var number :Int = 0
+    var number2: Int = 0
+    
+    
+    var operation : Int = 0
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
+    
+    
+    
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func select3(){
+            number = number*10+3
+        label.text = String(number)
+        
+        }
+    @IBAction func select4(){
+        number = number*10+4
+        label.text = String(number)
+    }
+    @IBAction func plus(){
+        label.text = String(0)
+        operation = 1
+        number2 = number
+        number = 0
+        
+    }
+    @IBAction func equal(){
+        label.text = String(number + number2)
+    }
 
-
+  
+   
 }
 
